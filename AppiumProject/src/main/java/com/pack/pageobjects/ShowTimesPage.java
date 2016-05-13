092e5803c3170f4ac1aa6abf4cff8cfb5326f65c
ganeshransarje <ganesh.ransarje@opexsoftware.com>
2016-05-13 20:22:31 +0530

    Changes for picking up future show

--- AppiumProject/src/main/java/com/pack/pageobjects/ShowTimesPage.java
+++ AppiumProject/src/main/java/com/pack/pageobjects/ShowTimesPage.java
@@ -2,6 +2,8 @@ package com.pack.pageobjects;
 
 import static org.testng.Assert.assertEquals;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 import org.openqa.selenium.By;
@@ -9,7 +11,7 @@ import org.openqa.selenium.WebElement;
 
 import com.pack.utilities.Utility;
 
-public class ShowTimesPage extends Utility{
+public class ShowTimesPage extends Utility {
 	public static final By titleId = By.id("com.bt.bms:id/txvTitle");
 	private static final By evbentNameId = By.id("com.bt.bms:id/eventName");
 	private static final By venueNameId = By.id("com.bt.bms:id/venueName");
@@ -19,70 +21,87 @@ public class ShowTimesPage extends Utility{
 	private static final By timeId = By.id("com.bt.bms:id/time");
 	private static final By timeModeId = By.id("com.bt.bms:id/time_mode");
 	private static final By categoryId = By.id("com.bt.bms:id/txvCatName");
-	private static final By categoryStatusId = By.id("com.bt.bms:id/txvCatStatus");
+	private static final By categoryStatusId = By
+			.id("com.bt.bms:id/txvCatStatus");
 	private static boolean isShowTimeAvailable = false;
 	private static boolean isSeatCatAvailable = false;
 	public static final String TIME = prop.getProperty("time");
 	public static final String TIMEMODE = prop.getProperty("timemode");
 	public static final String SEATCATEGORY = prop.getProperty("seatcategory");
-	
-	public static void verifyShowTimesPage(){
+
+	public static void verifyShowTimesPage() {
 		assertEquals(driver.findElement(titleId).getText(), "SHOW TIMES");
-		assertEquals(driver.findElement(evbentNameId).getText(), MoviesPage.MOVIENAME);
-		assertEquals(driver.findElement(venueNameId).getText(), PickVenuePage.VENUENAME);
+		assertEquals(driver.findElement(evbentNameId).getText(),
+				MoviesPage.MOVIENAME);
+		assertEquals(driver.findElement(venueNameId).getText(),
+				PickVenuePage.VENUENAME);
 		assertEquals(driver.findElement(dayId).getText(), PickVenuePage.DATE);
-		assertEquals(driver.findElement(monthId).getText(), PickVenuePage.MONTH.toUpperCase());
+		assertEquals(driver.findElement(monthId).getText(),
+				PickVenuePage.MONTH.toUpperCase());
 	}
-	
-	public static Boolean selectShowTime(){
+
+	public static Boolean selectShowTime() {
 		List<WebElement> timeElements = driver.findElements(timeId);
-//		List<WebElement> timeModeElements = driver.findElements(timeModeId);
-//		for(int i=0; i<timeElements.size(); i++){
-//			if(timeElements.get(i).getText().equalsIgnoreCase(TIME) && timeModeElements.get(i).getText().equalsIgnoreCase(TIMEMODE)){
-//				isShowTimeAvailable = true;
-//				clickOnElement(timeElements.get(i));
-//				break;
-//			}
-//		}
-		
-		if(timeElements.size()>0){
-			clickOnElement(timeElements.get(0));
-			System.out.println("Show Time Available");
-			isShowTimeAvailable = true;
+		// List<WebElement> timeModeElements = driver.findElements(timeModeId);
+		// for(int i=0; i<timeElements.size(); i++){
+		// if(timeElements.get(i).getText().equalsIgnoreCase(TIME) &&
+		// timeModeElements.get(i).getText().equalsIgnoreCase(TIMEMODE)){
+		// isShowTimeAvailable = true;
+		// clickOnElement(timeElements.get(i));
+		// break;
+		// }
+		// }
+
+		if (timeElements.size() > 0) {
+			for (WebElement element : timeElements) {
+				Calendar calendar = new GregorianCalendar();
+				int hour = calendar.get(Calendar.HOUR);
+				String[] e = element.getText().split(":");
+
+				if (Integer.parseInt(e[0]) > hour + 1) {
+					clickOnElement(element);
+					System.out.println("Show Time Available");
+					isShowTimeAvailable = true;
+					break;
+				}
+			}
 		}
-		
-		if(!isShowTimeAvailable){
+		if (!isShowTimeAvailable) {
 			System.out.println("Show Time not available");
 			return false;
-		}else
+		} else
 			return true;
 	}
-	
-	public static Boolean selectSeatCategory(){
+
+	public static Boolean selectSeatCategory() {
 		List<WebElement> categoryElements = driver.findElements(categoryId);
-		List<WebElement> categoryStatusElements = driver.findElements(categoryStatusId);
-//		for(int i=0; i<categoryElements.size(); i++){
-//			if(categoryElements.get(i).getText().equalsIgnoreCase(SEATCATEGORY) && categoryStatusElements.get(i).getText().equalsIgnoreCase("available")){
-//				isSeatCatAvailable = true;
-//				clickOnElement(categoryElements.get(i));
-//				waitForElement(HowManyTicketsPage.titleId);
-//				break;
-//			}
-//		}
-		
-		for(int i=0; i<categoryStatusElements.size(); i++){
-			if(categoryStatusElements.get(i).getText().equalsIgnoreCase("available")){
+		List<WebElement> categoryStatusElements = driver
+				.findElements(categoryStatusId);
+		// for(int i=0; i<categoryElements.size(); i++){
+		// if(categoryElements.get(i).getText().equalsIgnoreCase(SEATCATEGORY)
+		// &&
+		// categoryStatusElements.get(i).getText().equalsIgnoreCase("available")){
+		// isSeatCatAvailable = true;
+		// clickOnElement(categoryElements.get(i));
+		// waitForElement(HowManyTicketsPage.titleId);
+		// break;
+		// }
+		// }
+
+		for (int i = 0; i < categoryStatusElements.size(); i++) {
+			if (categoryStatusElements.get(i).getText()
+					.equalsIgnoreCase("available")) {
 				isSeatCatAvailable = true;
 				clickOnElement(categoryElements.get(i));
 				waitForElement(HowManyTicketsPage.titleId);
 				break;
 			}
 		}
-		
-		if(!isSeatCatAvailable){
+
+		if (!isSeatCatAvailable) {
 			System.out.println("Seat Category not available");
 			return false;
-		}else
+		} else
 			return true;
 	}
 }
